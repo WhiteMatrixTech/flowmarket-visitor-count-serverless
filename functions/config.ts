af26bed1baa090be552b85bf95dc688262a2739e
牛叠谊 <18715156450@163.com>
2022-10-12 17:54:10 +0800

    fix: private-key line-break error

--- functions/config.ts
+++ functions/config.ts
@@ -9,7 +9,7 @@ export default {
   propertyId: '335033272',
   auth: {
     projectId: 'market-visit-count',
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey: (process.env.PRIVATE_KEY as string).replace(/\\n/gm, '\n'),
     clientEmail: 'account1@market-visit-count.iam.gserviceaccount.com',
   },
   startDate: '2010-01-01',
